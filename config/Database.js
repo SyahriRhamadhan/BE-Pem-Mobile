3663e4994759aba061ba85d6051760f8868c15a7
Syahri Rhamadhan <2001020038@student.umrah.ac.id>
2023-06-09 16:24:46 +0700

    configure db mysql

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize('railway', 'root', 'qoX5nqCDnhKPJpdBX2EL', {
-    host: "containers-us-west-152.railway.app",
-    port: "7427",
+const db = new Sequelize('railway', 'root', 'TDFb9uW2JVDXCiahCdqy', {
+    host: "containers-us-west-164.railway.app",
+    port: "7497",
     dialect: "mysql"
 });
 export default db;
